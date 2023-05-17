a1a87f2a3b7587b6168309be705d97501bfec1f3
Mohammad Yassine <115090892+ausminatalli@users.noreply.github.com>
2023-05-17 16:04:32 +0300

    View Driver implementation

--- admin/viewdriver.php
+++ admin/viewdriver.php
@@ -7,6 +7,88 @@
     <title>Skyline View Drivers</title>
   </head>
   <body>
-    <p>View Driver</p>
+    <style>
+      .btn {
+        font-size: 17px;
+      }
+
+      #myTable {
+        margin-bottom: 0px;
+      }
+
+      .green {
+        background-color: green !important;
+        border: 1px solid;
+        width: 30px;
+      }
+
+      .red {
+        background-color: red !important;
+        border: 1px solid;
+        width: 30px;
+      }
+
+      .action-buttons {
+        display: flex;
+        gap: 10px;
+      }
+    </style>
+    <div>
+      <h2 class="text-center">View Driver</h2>
+      <table id="myTable" class="table table-striped" style="width: 100%">
+        <thead>
+          <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>MobileNumber</th>
+            <th>Email</th>
+            <th>Station</th>
+            <th>Licensedate</th>
+            <th>LicenseEx</th>
+            <th>Online</th>
+            <th>Action</th>
+            <!-- Added column for the block button -->
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+            $jsonData = file_get_contents('demodata/driver.json');
+            $data = json_decode($jsonData, true);
+
+            foreach ($data as $row) {
+              $onlineClass = $row['Online'] == 1 ? 'green' : 'red';
+              $licenseClass = $row['LicenseExpired'] == 1 ? 'green' : 'red';
+              echo "<tr>";
+              echo "<td>".$row['Firstname']."</td>";
+              echo "<td>".$row['Lastname']."</td>";
+              echo "<td>".$row['MobileNumber']."</td>";
+              echo "<td>".$row['Email']."</td>";
+              echo '<td>';
+              echo '<select class="station-dropdown">';
+              
+              
+              $stationData = ['Tripoli', 'Beirut', 'Saida'];
+              foreach ($stationData as $station) {
+                $selected = $station == $row['Station'] ? 'selected' : '';
+                echo '<option value="'.$station.'" '.$selected.'>'.$station.'</option>';
+              }
+
+              echo '</select>';
+              echo '</td>';
+              echo '<td><input type="date" value="'.$row['Licensedate'].'"></td>';
+              echo '<td class="'.$licenseClass.'"></td>';
+              echo '<td class="'.$onlineClass.'"></td>';
+              echo '<td>';
+              echo '<div class="action-buttons">';
+              echo '<button class="btn btn-warning">Edit</button>';
+              echo '<button class="btn btn-danger">Delete</button>';
+              echo '</div>';
+              echo '</td>';
+              echo "</tr>";
+            }
+          ?>
+        </tbody>
+      </table>
+    </div>
   </body>
 </html>
