99ea6f7b187f49ac8cdcc357e06657965731c65d
hassan barada <baradahassan30@gmail.com>
2023-05-17 16:41:15 +0300

    add bus

--- admin/viewbus.php
+++ admin/viewbus.php
@@ -1,12 +1,54 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
   <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+   
   </head>
+  <style>
+    .btn {
+      font-size: 17px;
+    }
+
+    #myTable {
+      margin-bottom: 0px;
+    }
+  </style>
   <body>
-    <p>View Bus page</p>
+  
+  <div>
+    <table id="myTable" class="table table-striped" style="width: 100%">
+      <thead>
+        <tr>
+          <th>Driver name</th>
+          <th>capacity</th>
+          <th>Station</th>
+          <th>Mechanicdue date</th>
+          <th>Insurance number</th>
+          <th>Accidents number</th>
+          <th>Action</th>
+        
+          <!-- Added column for the block button -->
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+          $jsonData = file_get_contents('demodata/bus.json');
+          $data = json_decode($jsonData, true);
+
+          foreach ($data as $row) {
+            echo "<tr>";
+            echo "<td>".$row['Driver name']."</td>";
+            echo "<td>".$row['capacity']."</td>";
+            echo "<td>".$row['Station']."</td>";
+            echo "<td>".$row['Mechanicdue date']."</td>";
+            echo "<td>".$row['Insurance number']."</td>";
+            echo "<td>".$row['Accidents number']."</td>";
+            echo '<td><button class="btn btn-warning mr-2">Edit</button><button class="btn btn-danger">Delete</button></td>';
+            echo "</tr>";
+          }
+        ?>
+      </tbody>
+    </table>
+  </div>
+
   </body>
 </html>
