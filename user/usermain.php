595a0eb8a968e293e4bbdd939f7fa52e947d50e3
Mohammad Yassine <115090892+ausminatalli@users.noreply.github.com>
2023-05-27 13:27:10 +0300

    add lira USD

--- user/usermain.php
+++ user/usermain.php
@@ -13,20 +13,50 @@
     />
     <title>Skyline User Page</title>
   </head>
-  <body>
+  <body style=''>
     <style>
   #map{
-        width: 100%;
-        height: 320px;
+    width: 100%;
+        height: 400px;
+      }
+      .scrollable-container {
+            height: 400px;
+            overflow: auto;
+            padding: 0 10px;
+        }
+        /*       ScrollBar 1        */
+        
+        .scrollable-container::-webkit-scrollbar {
+            width: 16px;
+        }
+        
+        .scrollable-container::-webkit-scrollbar-track {
+            border-radius: 8px;
+            background-color: #e7e7e7;
+            border: 1px solid #cacaca;
+        }
+        
+        .scrollable-container::-webkit-scrollbar-thumb {
+            border-radius: 8px;
+          border: 3px solid transparent;
+           background-clip: content-box;
+            background-color: #d55959;
+        }
+        #result-container{
+          margin-left:-15px;
+        }
+        @media (max-width: 767px) {
+        .row.flex-column-reverse-sm {
+          flex-direction: column-reverse !important;
+        }
       }
       </style>
     <?php   
    include('header.html');
-
     ?>
     <div class="usermain">
-    <section class="filter" style="padding-top: 60px">
-      <div class="filter-contant container">
+    <section class="filter" style='padding-top:100px'>
+    <div class="filter-contant container">
         <form class="form" action="#">
           <div class="form1">
             <div class="origin">
@@ -72,53 +102,55 @@
         </form>
       </div>
     </section>
-    <div class="row">
-        <div class="col-md-6">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Result:</h5>
-              <div class="resultcontainer">
-    <?php
-    
-    $data = json_decode(file_get_contents("../main/user.json"), true);
 
-    
-    foreach ($data as $item) {
-    ?>
-    <div class="box">
-      <div class="leftsection">
-        <div class="firstrow">
-          <i class="fa-solid fa-location-dot fa-xs" id="uppericon"></i>
-          <h5 class="from"><?php echo $item['from']; ?></h5>
-          <h5 class="origin"><?php echo $item['origin']; ?></h5>
-        </div>
-        <div class="secondrow">
-          <i class="fa-solid fa-location-dot fa-xs" id="bottomicon"></i>
-          <h5 class="to"><?php echo $item['to']; ?></h5>
-          <h5 class="destination"><?php echo $item['destination']; ?></h5>
-        </div>
-        <div class="thirdrow">
-          <i class="fa-sharp fa-solid fa-bus fa-sm"></i>
-          <h5 class="totaltime"><?php echo $item['totaltime']; ?></h5>
-        </div>
-      </div>
-      <div class="rightsection">
-        <h5><?php echo $item['price']; ?></h5>
-        <a href="#">
-          Book
-        </a>
-      </div>
-    </div>
-    <?php
-    }
-    ?>
-  </div>
+    <div class="container mt-4">
+        <div class="row flex-column-reverse-sm">
+            <h5 class="card-title mb-2 mt-lg-0 mt-sm-4">Result: 6</h5>
+            <div class="col-md-6">
+                <div class="card mt-lg-0 mt-sm-4">
+                    <div class="card-body scrollable-container " >
+                      
+                        <div id="result-container" >
+                            <?php
+                            $data = json_decode(file_get_contents("../main/user.json"), true);
+                                 
+                            foreach ($data as $item) {
+                              
+                                ?>
+                                 <div class="box">
+                                    <div class="leftsection">
+                                        <div class="firstrow">
+                                            <i class="fa-solid fa-location-dot fa-xs" id="uppericon"></i>
+                                            <h5 class="from"><?php echo $item['from']; ?></h5>
+                                            <h5 class="origin"><?php echo $item['origin']; ?></h5>
+                                        </div>
+                                        <div class="secondrow">
+                                            <i class="fa-solid fa-location-dot fa-xs" id="bottomicon"></i>
+                                            <h5 class="to"><?php echo $item['to']; ?></h5>
+                                            <h5 class="destination"><?php echo $item['destination']; ?></h5>
+                                        </div>
+                                        <div class="thirdrow">
+                                            <i class="fa-sharp fa-solid fa-bus fa-sm"></i>
+                                            <h5 class="totaltime"><?php echo $item['totaltime']; ?></h5>
+                                        </div>
+                                    </div>
+                                    <div class="rightsection">
+                                        <h5><?php echo $item['price']; ?></h5>
+                                        <a href="#">
+                                            Select
+                                        </a>
+                                    </div>
+                                </div>
+                                <?php
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-        <div class="col-md-5">
-          <div id="map"></div>
-        </div>
+            <div class="col-md-6">
+                <div id="map" class="card"></div>
+            </div>
       </div>
       <div class="row">
       <div class="card shadow-lg rounded-lg">
