1f1b2a25cfd62eb217d420a2d8397d66059abdae
Mohammad Yassine <115090892+ausminatalli@users.noreply.github.com>
2023-06-11 19:55:18 +0300

    search Update

--- api/main/functions.php
+++ api/main/functions.php
@@ -4,54 +4,7 @@
 function getTrips($conn,$condition,$data)
 {
  
- $query;
-if($condition=='All')
-{
     $query = "SELECT
-        `skyline`.`trips`.`tripid` AS `tripid`,
-        `skyline`.`station`.`provincename` AS `origin`,
-        `station2`.`provincename` AS `destination`,
-        `skyline`.`station`.`stationname` AS `originlocation`,
-        `station2`.`stationname` AS `destinationlocation`,
-        DATE_FORMAT(`skyline`.`trips`.`schedule`, '%Y-%m-%d') AS `schedule`,
-        TIME_FORMAT(`skyline`.`trips`.`movetime`, '%H:%i') AS `movetime`,
-        TIME_FORMAT(`skyline`.`trips`.`arrivetime`, '%H:%i') AS `arrivetime`,
-        TIME_FORMAT(TIMEDIFF(`skyline`.`trips`.`arrivetime`, `skyline`.`trips`.`movetime`), '%H:%i') AS `time_difference`,
-        `skyline`.`trips`.`ticketprice` AS `ticketprice`
-        FROM
-        `skyline`.`trips`
-        JOIN `skyline`.`station` ON (`skyline`.`station`.`stationid` = `skyline`.`trips`.`tripfrom`)
-        JOIN `skyline`.`station` `station2` ON (`station2`.`stationid` = `skyline`.`trips`.`tripto`)
-        WHERE
-        `skyline`.`station`.`provincename` = '" . $data[0]['origin'] . "' AND `station2`.`provincename` = '" . $data[0]['destination'] . "'
-        AND DATE(`skyline`.`trips`.`schedule`) = '" . $data[0]['tripdate'] . "' AND `movetime` = '" . $data[0]['triptime'] . "'";
-}
-    else if($condition=='triptime')
-    {
-        $query = "SELECT
-        `skyline`.`trips`.`tripid` AS `tripid`,
-        `skyline`.`station`.`provincename` AS `origin`,
-        `station2`.`provincename` AS `destination`,
-        `skyline`.`station`.`stationname` AS `originlocation`,
-        `station2`.`stationname` AS `destinationlocation`,,
-        DATE_FORMAT(`skyline`.`trips`.`schedule`, '%Y-%m-%d') AS `schedule`,
-        TIME_FORMAT(`skyline`.`trips`.`movetime`, '%H:%i') AS `movetime`,
-        TIME_FORMAT(`skyline`.`trips`.`arrivetime`, '%H:%i') AS `arrivetime`,
-        TIME_FORMAT(TIMEDIFF(`skyline`.`trips`.`arrivetime`, `skyline`.`trips`.`movetime`), '%H:%i') AS `time_difference`,
-        `skyline`.`trips`.`ticketprice` AS `ticketprice`
-        FROM
-        `skyline`.`trips`
-        JOIN `skyline`.`station` ON `skyline`.`station`.`stationid` = `skyline`.`trips`.`tripfrom`
-        JOIN `skyline`.`station` `station2` ON `station2`.`stationid` = `skyline`.`trips`.`tripto`
-        WHERE
-        `skyline`.`station`.`provincename` = '" . $data[0]['origin'] . "' AND `station2`.`provincename` = '" . $data[0]['destination'] . "'
-        AND `movetime` = '" . $data[0]['triptime'] . "'";
-
-   
-    }
-    else if($condition=='tripdate')
-    {
-        $query = "SELECT
     `skyline`.`trips`.`tripid` AS `tripid`,
     `skyline`.`station`.`provincename` AS `origin`,
     `station2`.`provincename` AS `destination`,
@@ -63,33 +16,30 @@ if($condition=='All')
     TIME_FORMAT(TIMEDIFF(`skyline`.`trips`.`arrivetime`, `skyline`.`trips`.`movetime`), '%H:%i') AS `time_difference`,
     `skyline`.`trips`.`ticketprice` AS `ticketprice`
     FROM
-    ((`skyline`.`trips`
-    JOIN `skyline`.`station` ON ((`skyline`.`station`.`stationid` = `skyline`.`trips`.`tripfrom`)))
-    JOIN `skyline`.`station` `station2` ON ((`station2`.`stationid` = `skyline`.`trips`.`tripto`))) 
-    WHERE
-    `skyline`.`station`.`provincename` = '" . $data[0]['origin'] . "' AND `station2`.`provincename` = '" . $data[0]['destination'] . "'
+    `skyline`.`trips`
+    JOIN `skyline`.`station` ON (`skyline`.`station`.`stationid` = `skyline`.`trips`.`tripfrom`)
+    JOIN `skyline`.`station` `station2` ON (`station2`.`stationid` = `skyline`.`trips`.`tripto`)
+    WHERE `skyline`.`station`.`provincename` = '" . $data[0]['origin'] . "' AND `station2`.`provincename` = '" . $data[0]['destination'] . "'";
+if($condition=='All')
+{
+    $query .= "
+        AND DATE(`skyline`.`trips`.`schedule`) = '" . $data[0]['tripdate'] . "' AND `movetime` = '" . $data[0]['triptime'] . "'";
+}
+    else if($condition=='triptime')
+    {
+        $query .= "
+        AND `movetime` = '" . $data[0]['triptime'] . "'";
+
+   
+    }
+    else if($condition=='tripdate')
+    {
+        $query .= "
     AND DATE(schedule) = '" . $data[0]['tripdate'] . "';";
 
     }
     else if($condition=='validated')
     {
-    $query = "SELECT
-    `skyline`.`trips`.`tripid` AS `tripid`,
-    `skyline`.`station`.`provincename` AS `origin`,
-    `station2`.`provincename` AS `destination`,
-    `skyline`.`station`.`stationname` AS `originlocation`,
-    `station2`.`stationname` AS `destinationlocation`,
-    `skyline`.`trips`.`schedule` AS `schedule`,
-    TIME_FORMAT(`skyline`.`trips`.`movetime`, '%H:%i') AS `movetime`,
-    TIME_FORMAT(`skyline`.`trips`.`arrivetime`, '%H:%i') AS `arrivetime`,
-    TIME_FORMAT(TIMEDIFF(`skyline`.`trips`.`arrivetime`, `skyline`.`trips`.`movetime`), '%H:%i') AS `time_difference`,
-    `skyline`.`trips`.`ticketprice` AS `ticketprice`
-    FROM
-    ((`skyline`.`trips`
-    JOIN `skyline`.`station` ON (`skyline`.`station`.`stationid` = `skyline`.`trips`.`tripfrom`))
-    JOIN `skyline`.`station` `station2` ON (`station2`.`stationid` = `skyline`.`trips`.`tripto`)) 
-    WHERE
-    `skyline`.`station`.`provincename` = '" . $data[0]['origin'] . "' AND `station2`.`provincename` = '" . $data[0]['destination'] . "'";
 
     }
     $result = mysqli_query($conn, $query);
@@ -114,12 +64,4 @@ if($condition=='All')
 
 return $searchResults;
 }
-
-
-
-
-
-
-
-
 ?>
