77ddb4c261662e0795ac9f7216f24c117bab9ce5
Mohammad Yassine <115090892+ausminatalli@users.noreply.github.com>
2023-06-12 15:44:25 +0300

    admin Updates

---
+++ api/admin/adminfunctions.php
@@ -0,0 +1,16 @@
+<?php
+
+function AddTrip($conn,$data)
+{
+$query = "INSERT INTO skyline.trips (tripid,busid,tripfrom,tripto,status,schedule,movetime,arrivetime,ticketprice,details)
+values (2323,'9191','2','1','onset','2023-06-13','10:00:00','13:00:00','20000','sddsssdds')"
+
+
+    
+}
+
+
+
+
+
+?>
