81f591f7a2caf2327c40b2b6c460a3fb34fbaca5
hassan barada <baradahassan30@gmail.com>
2023-06-17 14:03:10 +0300

    vacation request and add session in processlogin.php

--- driver/DayOff.php
+++ driver/DayOff.php
@@ -17,36 +17,48 @@ include('../path.php')
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"> 
   </head>
   <body>
+
   <?php include('../include/driverheader.html')  ?>
+  <?php     
+  
+  session_start();
+  if(isset($_SESSION['id']) && ($_SESSION['type']==1)){
+    $name=$_SESSION['name'];
+    $email=$_SESSION['email'];
+    $mobilenumber=$_SESSION['mobile'];
+  }
+  
+  
+  ?>
     <div class="DayOff">
       <h1>Day Off Request Form</h1>
-      <form action="" class="Input">
+      <form action="vacation.php" method="post" class="Input">
         <div class="flex-input">
           <label for="name">Full Name:</label>
-          <input type="text" id="name" />
+          <input type="text" id="name" value="<?php echo $name?>" disabled/>
         </div>
         <div class="flex-input">
           <label for="email">Email Address:</label>
-          <input type="text" id="email"  />
+          <input type="text" id="email" value="<?php echo $email?>" disabled />
         </div>
         <div class="flex-input">
           <label for="phoneNumber">Phone Number:</label>
-          <input type="text" id="phoneNumber"  />
+          <input type="text" id="phoneNumber" value="<?php echo $mobilenumber?>" disabled />
         </div>
         <div class="flex gap-2" style="display: flex; align-items: center;">
 
           <div class="flex-input w-100">
             <label for="startDate">Start Date:</label>
-            <input class="w-100" type="date" id="startDate"  />
+            <input class="w-100" type="date" id="startDate" name="startdate"/>
           </div>
           <div class="flex-input w-100">
             <label for="endDate">End Date:</label >
-            <input type="date" id="endDate" />
+            <input type="date" id="endDate" name="enddate"/>
           </div>
         </div>
         <div class="flex-input">
           <label for="reason">Reason for Day Off:</label>
-          <textarea id="reason" rows="5"></textarea>
+          <textarea id="reason" rows="5" name="reason"></textarea>
         </div>
         <button type="submit">Submit</button>
       </form>
