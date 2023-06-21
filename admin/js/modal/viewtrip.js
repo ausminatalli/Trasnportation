5a52f5faaf5d3e80619d80e1439105ce48336926
Mohammad Yassine <115090892+ausminatalli@users.noreply.github.com>
2023-06-21 21:03:44 +0300

    trips bug

--- admin/js/modal/viewtrip.js
+++ admin/js/modal/viewtrip.js
@@ -1,4 +1,3 @@
-function Edittrip(){
 $(document).ready(function() {
   $(document).on('click', '.btn-edit', function() {
     var tripId = $(this).data('tripid');
@@ -21,7 +20,7 @@ $(document).ready(function() {
     $('#editModal').find('form')[0].reset();
   });
 
-  $('#editModal').on('click', '.btn-primary', function(e) {
+  $(document).on('click', '.btn-primary', function(e) {
     e.preventDefault();
 
     var tripId = $(this).data('tripid');
@@ -29,7 +28,7 @@ $(document).ready(function() {
     var startTime = $('#editModal #startTime').val();
     var arriveTime = $('#editModal #arriveTime').val();
     $('#editModal').modal('hide');
-     //Perform AJAX request to save changes
+    // Perform AJAX request to save changes
     $.ajax({
       url: '../api/admin/editform/edittrip.php',
       method: 'POST',
@@ -40,109 +39,41 @@ $(document).ready(function() {
         arriveTime: arriveTime,
       },
       success: function(response) {
-        
-        $('#editModal').modal('hide');
+        // $('#editModal').modal('hide');
         location.reload();
       },
       error: function(xhr, status, error) {
-        console.log("error=>",error)
+        console.log('error=>', error);
         // ...
       }
     });
   });
-});
 
-}
+  $(document).on('click', '.btn-delete1', function() {
+    var tripId = $(this).data('tripid');
+    var deleteButton = $(this);
+    $('#deleteConfirmationModal').modal('show');
 
-
-/*function deleteTrip() {
-  let modal = $(".modal-container");
-  let btn = $(".btn-delete1");
-  let confirmBtn = $(".btn-confirm");
-  let tripId = btn.data("tripid"); // Get the tripid from the button's data attribute
-
-  let closeBtn = $(".btn");
-
-  // EventListener
-  btn.on("click", function() {
-    modal.addClass("show");
-  });
-
-  confirmBtn.on("click", function() {
-    // Send AJAX request
-    $.ajax({
-      url: "../api/admin/deleteform/deletetrip.php",
-      method: "POST",
-      data: { tripid: tripId },
-      success: function(response) {
-        // Handle the response from the server
-        console.log(response);
-      },
-      error: function(xhr, status, error) {
-        // Handle the error
-        console.log(error);
-      }
-    });
-
-    modal.removeClass("show");
-  });
-
-  closeBtn.each(function() {
-    $(this).on("click", function() {
-      modal.removeClass("show");
-    });
-  });
-
-  $(window).on("click", function(event) {
-    if (event.target == modal[0]) {
-      modal.removeClass("show");
-    }
-  });
-}
-
-  */
-  
-
-
-
-
-
-  Edittrip();
- 
-  $(document).ready(function() {
-   
-    $(document).on("click", ".btn-delete1", function() {
-      var tripId = $(this).data("tripid");
-  
-      
-      var deleteButton = $(this);
-  
-      
-      $("#deleteConfirmationModal").modal("show");
-  
-      $("#confirmDeleteBtn").on("click", function() {
-        $.ajax({
-          url: "../api/admin/deleteform/deletetrip.php",
-          method: "POST",
-          data: { tripid: tripId },
-          success: function(response) {
-            console.log(response);
-            if (response === "Trip Cannot be deleted") {
-              window.location.href = "http://localhost/transportation/admin?msg=tripfailed";
-            } else {
-              
-              deleteButton.closest('tr').remove();
-            }
-  
-            $("#deleteConfirmationModal").modal("hide");
-          },
-          error: function(xhr, status, error) {
-
-            console.log(error);
+    $('#confirmDeleteBtn').off().on('click', function() {
+      $.ajax({
+        url: '../api/admin/deleteform/deletetrip.php',
+        method: 'POST',
+        data: { tripid: tripId },
+        success: function(response) {
+          console.log(response);
+          if (response === 'Trip Cannot be deleted') {
+            window.location.href = 'http://localhost/transportation/admin?msg=tripfailed';
+          } else {
+            deleteButton.closest('tr').remove();
           }
-        });
+          $('#deleteConfirmationModal').modal('hide');
+        },
+        error: function(xhr, status, error) {
+          console.log(error);
+        }
       });
     });
   });
-  
-  
+});
+
+
